05acab19b37499510e074a68e3f740af120c8221
yunerkis <yumycary@gmail.com>
2017-08-27 17:24:16 -0500

    Remove Extra

--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { IndexRoute, Route } from 'react-router';
 import App from 'app/components/App';
 import Home from 'app/components/Home';
-import About from 'app/components/About';
 
 export default (
     <Route component={App} path="/">
         <IndexRoute component={Home} />
-        <Route component={About} path="about" />
     </Route>
 );

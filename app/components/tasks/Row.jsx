4a46ba815a169898e624b978a74027ab0bebdc24
yunerkis <yumycary@gmail.com>
2017-08-27 17:10:00 -0500

    Update by Id

--- app/components/tasks/Row.jsx
+++ app/components/tasks/Row.jsx
@@ -13,33 +13,30 @@ const UserRow = createClass({
     },
 
     onClickUser() {
-        const { description, onClickUser } = this.props;
-
-        onClickUser(description);
+        const { _id, onClickUser } = this.props;
+        onClickUser(_id);
     },
 
     onRemove() {
         const { _id, onRemoveUser } = this.props;
-
         onRemoveUser({ _id });
     },
 
     render() {
-        const { description, name, selectedUser, dateCreate, dateUpdate } = this.props;
+        const { description, name, selectedUser, dateCreated, dateUpdate, _id } = this.props;
 
         const classNames = css('users__row', {
-            'users__row--active': selectedUser === description
+            'users__row--active': selectedUser === _id
         });
 
         return (
             <tr className={classNames} onClick={this.onClickUser}>
                 <td>{name}</td>
                 <td>{description}</td>
-                <td>{dateCreate}</td>
+                <td>{dateCreated}</td>
                 <td>{dateUpdate}</td>
                 <td>
                     <button onClick={this.onRemove}>Remove</button>
-                    <button onClick={this.onRemove}>Update</button>
                 </td>
             </tr>
         );
